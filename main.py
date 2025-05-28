2df18619e16faabd0d9676cc9f20b2d503eef0c0
marosimarcell13 <marosimarcell13@gmail.com>
2025-05-28 22:54:20 +0200

    Scrren added with pygame

--- main.py
+++ main.py
@@ -2,9 +2,27 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+from constants import *
 
 def main():
+    #Initalize pygame
+    pygame.init()
+
+    #Create display with SCREEN_WIDTH and SCREEN_HEIGHT form constants
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")        
+
+    pygame.display.flip()
+
     print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
 
 if __name__ == "__main__":
     main()
