b3b8b8c1d8f7fd62e76a7533054453546fd62c38
marosimarcell13 <marosimarcell13@gmail.com>
2025-05-29 09:28:40 +0200

    Player ship move command added

--- main.py
+++ main.py
@@ -3,8 +3,12 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 
 def main():
+    #Instantiate player
+    player = Player(SCREEN_WIDTH / 2,SCREEN_HEIGHT / 2)
+
     #Initalize pygame
     pygame.init()
 
@@ -15,15 +19,28 @@ def main():
     #Create display with SCREEN_WIDTH and SCREEN_HEIGHT form constants
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        #Fill the screen background with black
         screen.fill("black")
-        dt = clock.tick(60)/1000        
 
-    pygame.display.flip()
+        #Rotate player
+        player.update(dt)
+
+        #Draw player
+        player.draw(screen)
+
+        #Refresh screen
+        pygame.display.flip()
+
+        #Get time delta since last refresh
+        dt = clock.tick(60)/1000       
+        print(dt) 
+
 
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
